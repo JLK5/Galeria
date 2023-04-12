eb1944f776b8694e1ed88f4e9245359bd3e0a902
JLK5 <jlkborkowski@gmail.com>
2023-04-12 09:45:40 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function showOverlay() {
+    let url = document.querySelector("div#bigPicture img").src;
+    document.querySelector("div#fullScreenOverlay img").src = url;
     //pokazuje zdjęcie na całym ekranie
     let overlay = document.getElementById("fullScreenOverlay");
     overlay.style.display = "flex"
@@ -9,5 +11,39 @@ function hideOverlay() {
     let overlay = document.getElementById("fullScreenOverlay");
     overlay.style.display = "none"
 }
-document.getElementById("bigPictureImg").addEventListener("click", showOverlay);
-document.getElementById("fullScreenImg").addEventListener("click", hideOverlay);
+function getRandomFromRange(min, max) {
+        //losujemy wartość z zakresu <0;1>
+        let random = Math.random();
+        //wyliczamy losową szerokość według kryteriów
+        let range = max - min;
+        //losowa liczba z zakresu 0-600
+        let randomFromRange = random * range;
+        //zaokrąglamy do liczby całkowitej
+        randomFromRange = Math.round(randomFromRange)
+        random = randomFromRange + min
+        return random;
+}
+function getRandomImageUrl() {
+    const minWidth = 1900;
+    const maxWidth = 2500;
+    let randomWidth = getRandomFromRange(minWidth, maxWidth);
+    const minHeight = 900;
+    const maxHeight = 1200;
+    let randomHeight = getRandomFromRange(minHeight, maxHeight);
+    let url = "https://picsum.photos/" + randomWidth + "/" + randomHeight;
+    return url; 
+}
+function thumbnailClick(event) {
+    let url = event.srcElement.src;
+    document.querySelector("div#bigPicture img").src = url;
+}
+document.getElementById("bigPicture").addEventListener("click", showOverlay);
+document.getElementById("fullScreenOverlay").addEventListener("click", hideOverlay);
+let imageList = document.getElementsByClassName("randomImage");
+for(let i = 0; i < imageList.length; i++) {
+    imageList[i].src = getRandomImageUrl();
+}
+let thumbnailList = document.querySelectorAll("div#thumbnailRoll img");
+for(let i = 0; i < thumbnailList.length; i++) {
+    thumbnailList[i].addEventListener('click', thumbnailClick);
+}
